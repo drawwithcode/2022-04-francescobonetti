89520057105c1d5bd8f3bb614bebbaa66a72d13a
francescobonetti <84150908+francescobonetti@users.noreply.github.com>
2022-11-22 23:54:07 +0100

    Project

--- sketch.js
+++ sketch.js
@@ -1,16 +1,99 @@
 function preload() {
-	// put preload code here
+  bebasNeue = loadFont('BebasNeue-Regular.ttf');
 }
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
-	const message = "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
-	textAlign(CENTER, CENTER);
-  textSize(16)
-	text(message, width/2, height/2);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  angleMode(DEGREES)
+
+  explainerText = createP("Ever thought about 3D drawing?");
 }
 
+
+let Line = [];
+let angle= 0;
+let rotationXstart;
+let rotationYstart;
+let rotationZstart;
+let notouch = true;
+let explainerText;
+let cover;
+let myMessage = "draw something";
+
 function draw() {
-	// put drawing code here
+  background("white");
+  noFill()
+  stroke("black")
+  strokeWeight(5)
+
+  if(notouch == true) {
+    angle = angle + 2;
+  }
+
+   if (mouseIsPressed) {
+   
+    angle = angle + 2;
+     
+    let x = (mouseX - width/2) * cos(angle); 
+    let y = mouseY - height/2;
+    let z = (mouseX - width/2) * sin(angle)
+    
+    Line.push([x,y,z])
+
+    rotationXstart = rotationX
+    rotationYstart = rotationY
+    rotationZstart = rotationZ
+   
+  } else if(notouch == false) {
+
+    angle+=0.5;
+
+    rotateX(rotationX-rotationXstart);
+    rotateY(rotationY-rotationYstart);
+    rotateZ(rotationZ-rotationZstart);
+  }
+
+  
+
+  if (angle == 360) {angle = 0};
+  rotateY(angle);
+
+  beginShape();
+
+  for (let i=0; i< Line.length; i++) {
+    let [x, y, z] = Line[i];
+    vertex(x, y, z);
+  }
+
+  endShape(); 
+  
+  strokeWeight(1)
+  line(width/2, 0, 0, -width/2, 0, 0);
+  line(0, height/3, 0, 0, -height/3, 0);  
+
+  fill("black")
+  textFont(bebasNeue)
+  textSize(32)
+  textLeading(32)
+  textAlign(CENTER)
+  text(myMessage, 0, -height/2+100);
+
 }
+
+function mousePressed(){
+  Line = [];
+  notouch = false
+}
+
+// ask for permissions on iOS
+function touchEnded(event) {
+  myMessage = "tilt to take a look at your work"  
+  
+  // check that those functions exist
+  // if they exist it means we are
+  // on iOS and we can request the permissions
+  if(DeviceOrientationEvent && DeviceOrientationEvent.requestPermission){
+    DeviceOrientationEvent.requestPermission()
+  }
+}
+
